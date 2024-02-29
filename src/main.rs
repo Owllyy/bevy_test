0be1909f08c2146d32999af3a5810901e2a69e8a
DoomDuck <docdoomduck@protonmail.com>
2024-02-29 12:02:33 +0100

    feat: Add global configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_cursor::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_inspector_egui::{
+    inspector_options::ReflectInspectorOptions, quick::ResourceInspectorPlugin, InspectorOptions,
+};
 use bevy_xpbd_2d::{
     components::{LinearDamping, LinearVelocity, Mass, RigidBody},
     plugins::{collision::Collider, PhysicsPlugins},
@@ -20,23 +22,43 @@ struct Dash {
 #[derive(Default, Component)]
 struct Direction(Vec2);
 
-const ACCELERATION: f32 = 6000.0;
-const DECCELERATION: f32 = 4000.0;
-const MAX_SPEED: f32 = 800.0;
-const DASH_SPEED: f32 = 2000.0;
-const DASH_DECELRATION: f32 = 200000.0;
-const DASH_DURATION: f32 = 0.1;
+#[derive(Reflect, Resource, InspectorOptions)]
+#[reflect(Resource, InspectorOptions)]
+struct Configuration {
+    acceleration: f32,
+    decceleration: f32,
+    max_speed: f32,
+    dash_speed: f32,
+    dash_decelration: f32,
+    dash_duration: f32,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            acceleration: 6000.0,
+            decceleration: 4000.0,
+            max_speed: 800.0,
+            dash_speed: 2000.0,
+            dash_decelration: 200000.0,
+            dash_duration: 0.1,
+        }
+    }
+}
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, TrackCursorPlugin))
         .add_plugins(PhysicsPlugins::default())
-        .add_plugins(WorldInspectorPlugin::default())
+        // .add_plugins(WorldInspectorPlugin::default())
+        .add_plugins(ResourceInspectorPlugin::<Configuration>::default())
+        .init_resource::<Configuration>()
         .add_systems(Startup, setup)
         .add_systems(
             Update,
             (keyboard_input_system, look_cursor, apply_force, dash).chain(),
-        ).register_type::<Player>()
+        )
+        .register_type::<Player>()
         .run();
 }
 
@@ -47,9 +69,7 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
-        Player {
-            speed: Vec2::ZERO,
-        },
+        Player { speed: Vec2::ZERO },
         Direction(Vec2::default()),
         RigidBody::Kinematic,
         Collider::circle(0.5),
@@ -98,21 +118,24 @@ fn look_cursor(cursor: Res<CursorLocation>, mut query: Query<&mut Transform, Wit
 
 fn dash(
     time: Res<Time>,
+    configurition: Res<Configuration>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut command: Commands,
-    mut query:  Query<(Entity, &Direction, Option<&mut Dash>)>,
+    mut query: Query<(Entity, &Direction, Option<&mut Dash>)>,
 ) {
     for (id, dir, dash) in &mut query {
         if keyboard_input.just_pressed(KeyCode::Space) && dash.is_none() {
             command.entity(id).insert(Dash {
-                speed: dir.0.normalize_or_zero() * DASH_SPEED,
-                duration: DASH_DURATION,
+                speed: dir.0.normalize_or_zero() * configurition.dash_speed,
+                duration: configurition.dash_duration,
             });
         } else if let Some(mut dash) = dash {
             if dash.duration > 0.0 {
                 dash.duration -= time.delta_seconds();
             } else {
-                let force = dash.speed.normalize_or_zero() * DASH_DECELRATION * time.delta_seconds();
+                let force = dash.speed.normalize_or_zero()
+                    * configurition.dash_decelration
+                    * time.delta_seconds();
                 if force.length() >= dash.speed.length() {
                     dash.speed = Vec2::ZERO;
                     command.entity(id).remove::<Dash>();
@@ -126,6 +149,7 @@ fn dash(
 
 fn keyboard_input_system(
     time: Res<Time>,
+    configurition: Res<Configuration>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Player, &mut Direction)>,
 ) {
@@ -145,9 +169,12 @@ fn keyboard_input_system(
         }
 
         if direction.0 != Vec2::ZERO {
-            player.speed += direction.0.normalize_or_zero() * ACCELERATION * time.delta_seconds();
+            player.speed +=
+                direction.0.normalize_or_zero() * configurition.acceleration * time.delta_seconds();
         } else {
-            let force = player.speed.normalize_or_zero() * DECCELERATION * time.delta_seconds();
+            let force = player.speed.normalize_or_zero()
+                * configurition.decceleration
+                * time.delta_seconds();
             if force.length() > player.speed.length() {
                 player.speed = Vec2::ZERO;
             } else {
@@ -155,13 +182,13 @@ fn keyboard_input_system(
             }
         }
 
-        if player.speed.length() > MAX_SPEED {
-            player.speed = player.speed.normalize_or_zero() * MAX_SPEED;
+        if player.speed.length() > configurition.max_speed {
+            player.speed = player.speed.normalize_or_zero() * configurition.max_speed;
         }
     }
 }
 
-fn apply_force(mut query:  Query<(&mut Player, &mut LinearVelocity, Option<& Dash>)>) {
+fn apply_force(mut query: Query<(&mut Player, &mut LinearVelocity, Option<&Dash>)>) {
     for (player, mut velocity, dash) in &mut query {
         velocity.0 = player.speed;
         if let Some(dash) = dash {
@@ -169,4 +196,3 @@ fn apply_force(mut query:  Query<(&mut Player, &mut LinearVelocity, Option<& Das
         }
     }
 }
-
